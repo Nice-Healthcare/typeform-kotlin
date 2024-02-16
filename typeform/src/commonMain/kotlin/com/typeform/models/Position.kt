ef98fd34d5f3a7f1d9b119126dd2502f5bf1be6a
Richard Piazza <rpiazza@nice.healthcare>
2024-02-16 13:12:56 -0600

    Corrected First Position reference fallbacks.

--- typeform/src/commonMain/kotlin/com/typeform/models/Position.kt
+++ typeform/src/commonMain/kotlin/com/typeform/models/Position.kt
@@ -9,6 +9,17 @@ sealed class Position {
     data class ScreenPosition(val screen: Screen) : Position()
     data class FieldPosition(val field: Field, val group: Group?) : Position()
 
+    fun associatedScreen(): Screen? {
+        return when(this) {
+            is ScreenPosition -> {
+                this.screen
+            }
+            else -> {
+                null
+            }
+        }
+    }
+
     fun associatedField(): Field? {
         return when(this) {
             is FieldPosition -> {
