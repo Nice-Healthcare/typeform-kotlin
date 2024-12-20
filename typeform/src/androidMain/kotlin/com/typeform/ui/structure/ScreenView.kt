43c380380818d93a4cadd9c22eaf8fdd3ba1a2b1
Richard Piazza <rpiazza@nice.healthcare>
2024-12-20 12:02:07 -0600

    Updated Kotlin & Compose Dependencies

--- typeform/src/androidMain/kotlin/com/typeform/ui/structure/ScreenView.kt
+++ typeform/src/androidMain/kotlin/com/typeform/ui/structure/ScreenView.kt
@@ -11,7 +11,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import coil3.compose.AsyncImage
 import com.typeform.models.Position
 import com.typeform.models.Responses
 import com.typeform.models.TypeformException
